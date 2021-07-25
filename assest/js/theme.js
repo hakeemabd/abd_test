f130326714d1c10930602f8f7b79b732227d16a9
hakeemabd <77714173+hakeemabd@users.noreply.github.com>
2021-07-25 12:26:27 +0530

    Add files via upload

---
+++ assest/js/theme.js
@@ -0,0 +1,11 @@
+/*!
+
+ */
+(function($) {
+    "use strict";
+    $.fn.ratingThemes['krajee-fas'] = {
+        filledStar: '<i class="fas fa-star"></i>',
+        emptyStar: '<i class="far fa-star"></i>',
+        clearButton: '<i class="fas fa-minus-circle"></i>'
+    };
+})(window.jQuery);
